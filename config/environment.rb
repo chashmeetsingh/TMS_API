ae0abdf77aa0eee4f7e248f64fba3da35aa84b12
Chashmeet Singh <chashmeetsingh@gmail.com>
2016-07-15 19:45:40 +0530

    update api keys

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,5 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 # Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-app_environment_variables = File.join(Rails.root, 'config', 'api_keys.rb')
+app_environment_variables = File.join(Rails.root, 'config', 'api_keys.yml')
 load(app_environment_variables) if File.exists?(app_environment_variables)
